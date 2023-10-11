53886340c2cc80e02a82dc9eba2344f6dab55483
Paweł Kromołowski <p.kromolowski@do-it.co>
2023-10-11 16:29:31 +0200

    create Potion namespace & display shop items

---
+++ Potion.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <array>
+#include <string_view>
+#include <cassert>
+
+namespace Potion {
+	enum Type {
+		healing,
+		mana,
+		speed,
+		invisibility,
+
+		max_potion_type,
+	};
+
+	using namespace std::literals;
+	constexpr std::array cost{ 20, 30, 12, 50 };
+	constexpr std::array name{ "healing"sv, "mana"sv, "speed"sv, "invisibility"sv };
+
+	static_assert(std::size(cost) == Type::max_potion_type && "Cost array have incorrect length!");
+	static_assert(std::size(name) == Type::max_potion_type && "Names array have incorrect length!");
+}
--- main.cpp
+++ main.cpp
@@ -1,5 +1,17 @@
+#include "Potion.h"
+
 #include <iostream>
 
+void shop() {
+    std::cout << "Here is our selection for today:\n";
+
+    for (int i{ 0 }; i < Potion::max_potion_type; i++) {
+        std::cout << i << ") " << Potion::name[i] << " costs " << Potion::cost[i] << '\n';
+    }
+}
+
 int main() {
+    shop();
+
     return 0;
 }
